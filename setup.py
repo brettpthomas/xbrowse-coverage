46c3c4768d3c98ed8fd71dc99b84223c5495a680
Brett Thomas <brettpthomas@gmail.com>
2013-07-08 16:17:21 -0400

    Vanilla setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import os
+
+# TODO
+# long_description = open(os.path.dirname(os.path.abspath(__file__)) + '/README.md').read()
+
+setup(
+    name='xBrowseCoverage',
+    version='0.1dev',
+    packages=['xcoverage',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=".",
+)
